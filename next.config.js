be47715e52efe45f7ae37384351d664e1b3e2b14
cesar angulo <acludo2@gmail.com>
2025-06-28 13:34:54 -0700

    prisma fix?

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  // Add other Next.js config options here
-}
-
-export default nextConfig
